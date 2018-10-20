ae4ee70259b2fc5e3466690ab4bc87d5a0343dcc
Tiberiu <tiberiu.rosu@mementohub.com>
2018-10-20 13:30:44 +0300

    Added random string generator for booking id

--- database/factories/BookingFactory.php
+++ database/factories/BookingFactory.php
@@ -2,11 +2,24 @@
 
 use Faker\Generator as Faker;
 
+function randomString($length = 5)
+{
+    $str = "";
+    $characters = array_merge(range('A', 'Z'), range('a', 'z'), range('0', '9'));
+    $max = count($characters) - 1;
+    for ($i = 0; $i < $length; $i++) {
+        $rand = mt_rand(0, $max);
+        $str .= $characters[$rand];
+    }
+    return strtoupper($str);
+}
+
+
 $factory->define(App\Booking::class, function (Faker $faker) {
 
     return [
-        'booking_id' => $faker->randomElement(['KJASD', 'JDLSU', 'KDHWGB']),
-        'hotel_id'   => $faker->randomNumber(5),
-        'data'       => json_encode(["pepeni" => "spanac"]),
+        'booking_id' => randomString(),
+        'hotel_id'   => randomString(10),
+        'data'       => json_encode(["column1" => $faker->word, "column2" => $faker->word, "column3" => $faker->word]),
     ];
 });
