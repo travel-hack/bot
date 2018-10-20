edbb71dd43020136f15fe3392f52b9153ba6a878
Bogdan Cismariu <bogdan.cismariu@gmail.com>
2018-10-20 11:45:20 +0300

    more flexibility in identifying user commands

--- routes/botman.php
+++ routes/botman.php
@@ -10,8 +10,12 @@ $botman->hears('Hi', function ($bot) {
 $botman->hears('Start conversation', BotManController::class.'@startConversation');
 
 
-$botman->hears('my bookings', BookingController::class . '@myBookings');
-
-$botman->hears('show booking {id}', BookingController::class . '@showBookings');
-
-$botman->hears('cancel booking {id}', BookingController::class . '@cancelBookings');
+/**
+ * Bookings
+ */
+// list bookings
+$botman->hears('(show|list)?\s?(all)?\s?(my)?\s(bookings|books|reservations|rezervations|resa)', BookingController::class . '@myBookings');
+// show one booking
+$botman->hears('(show|display)? (booking|book|reservation|rezervation|resa) {id}', BookingController::class . '@showBookings');
+// cancel booking
+$botman->hears('(cancel|delete)? (booking|book|reservation|rezervation|resa) {id}', BookingController::class . '@cancelBookings');
