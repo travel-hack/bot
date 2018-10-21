9452dd76afb80d8d895979d0f6cec155a22beaf8
Bogdan Cismariu <bogdan.cismariu@gmail.com>
2018-10-21 06:08:11 +0300

    logging user info

--- app/Http/Middleware/NewPlayers.php
+++ app/Http/Middleware/NewPlayers.php
@@ -6,18 +6,23 @@ use BotMan\BotMan\Interfaces\Middleware\Received;
 use BotMan\BotMan\Messages\Incoming\IncomingMessage;
 use BotMan\BotMan\BotMan;
 use App\Player;
+use Log;
 
-class NewPlayers implements Received 
+class NewPlayers implements Received
 {
     public function received(IncomingMessage $message, $next, BotMan $bot)
     {
+        Log::info($bot->getUser());
+        Log::info($bot->getSenderId());
+        return $next($message);
+
         $user = $bot->getUser();
         $user_id = $user->getId();
         $firstname = $user->getFirstName();
         $lastname  = $user->getLastName();
-                
+
         $newUser = Player::where('facebook_id', $user_id)->get();
-        
+
         if($newUser->isEmpty()) {
             $newUser = new Player(
                 [
--- routes/botman.php
+++ routes/botman.php
@@ -6,9 +6,14 @@ use BotMan\BotMan\Middleware\Dialogflow;
 use App\Http\Middleware\UserInputLogger;
 use App\Http\Middleware\NewPlayers;
 
-//$dialogflow = Dialogflow::create('913d9d2423d74322a7af72d0ad47aafc')->listenForAction();
+
 $botman = resolve('botman');
+
+
+//$dialogflow = Dialogflow::create('913d9d2423d74322a7af72d0ad47aafc')->listenForAction();
 //$botman->middleware->received($dialogflow);
+
+
 $userInputLogger = new UserInputLogger();
 $newPlayers = new NewPlayers();
 
@@ -18,7 +23,7 @@ $botman->middleware->received($newPlayers);
 $botman->hears('Hi|Hello|Yo|Ola', function ($bot) {
     $bot->reply('Smells like a soon to be seasoned traveler :)');
 });
-$botman->hears('Start conversation', BotManController::class.'@startConversation');
+// $botman->hears('Start conversation', BotManController::class.'@startConversation');
 
 /**
  * Hotel search
@@ -32,9 +37,9 @@ $botman->hears('book.now {property_code}', HotelsController::class . '@bookNow')
  * Bookings
  */
 // list active bookings
-$botman->hears('(show|list)?\s?(my)?\s(bookings|books|reservations|rezervations|resa)', BookingController::class . '@myBookings');
+$botman->hears('(show|list)?\s?(me)?\s?(my)?\s(bookings|books|reservations|rezervations|resa)', BookingController::class . '@myBookings');
 // list all bookings
-$botman->hears('(show|list)?\s?all\s?(my)?\s(bookings|books|reservations|rezervations|resa)', BookingController::class . '@allMyBookings');
+$botman->hears('(show|list)?\s?(me)?\s?all\s?(my)?\s(bookings|books|reservations|rezervations|resa)', BookingController::class . '@allMyBookings');
 // show one booking
 $botman->hears('(show|display)? (booking|book|reservation|rezervation|resa) {id}', BookingController::class . '@showBookings');
 // cancel booking
