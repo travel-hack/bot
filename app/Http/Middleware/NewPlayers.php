87c25f93cc99dda99e53a8e3da8ac21ff9bd931c
Bogdan Cismariu <bogdan.cismariu@gmail.com>
2018-10-21 06:30:28 +0300

    using exceptions

--- app/Http/Middleware/NewPlayers.php
+++ app/Http/Middleware/NewPlayers.php
@@ -12,29 +12,33 @@ class NewPlayers implements Received
 {
     public function received(IncomingMessage $message, $next, BotMan $bot)
     {
-        Log::info($bot->getUser());
-        Log::info($bot->getSenderId());
-        return $next($message);
+        try {
+            Log::info($bot->getUser());
+            Log::info($bot->getSenderId());
+            return $next($message);
 
-        $user = $bot->getUser();
-        $user_id = $user->getId();
-        $firstname = $user->getFirstName();
-        $lastname  = $user->getLastName();
+            $user = $bot->getUser();
+            $user_id = $user->getId();
+            $firstname = $user->getFirstName();
+            $lastname  = $user->getLastName();
 
-        $newUser = Player::where('facebook_id', $user_id)->get();
+            $newUser = Player::where('facebook_id', $user_id)->get();
 
-        if($newUser->isEmpty()) {
-            $newUser = new Player(
-                [
-                    'facebook_id'    => $user_id,
-                    'firstname'      => $firstname,
-                    'lastname'       => $lastname,
-                    'avatar_url'     => "https://via.placeholder.com/50x50",
-                    'rating'         => 60,
-                    'bookings_total' => 0,
-                ]
-            );
-            $newUser->save();
+            if($newUser->isEmpty()) {
+                $newUser = new Player(
+                    [
+                        'facebook_id'    => $user_id,
+                        'firstname'      => $firstname,
+                        'lastname'       => $lastname,
+                        'avatar_url'     => "https://via.placeholder.com/50x50",
+                        'rating'         => 60,
+                        'bookings_total' => 0,
+                    ]
+                );
+                $newUser->save();
+            }
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
         }
         return $next($message);
     }
